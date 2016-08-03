7f80b8d9676254e31bae816de47ff9cf6b801126
Lucas Bartholemy <lucas.bartholemy@humhub.com>
2016-08-03 14:56:05 +0200

    fixed migration problem

--- migrations/m150317_131255_initial.php
+++ migrations/m150317_131255_initial.php
@@ -5,31 +5,37 @@ class m150317_131255_initial extends humhub\components\Migration
 
     public function up()
     {
-        $this->createTable('dropbox_post', array(
-            'id' => 'pk',
-            'message' => 'text NOT NULL',
-            'created_at' => 'datetime DEFAULT NULL',
-            'created_by' => 'int(11) DEFAULT NULL',
-            'updated_at' => 'datetime DEFAULT NULL',
-            'updated_by' => 'int(11) DEFAULT NULL',
-                ), '');
+
+        try {
+
+            $this->createTable('dropbox_post', array(
+                'id' => 'pk',
+                'message' => 'text NOT NULL',
+                'created_at' => 'datetime DEFAULT NULL',
+                'created_by' => 'int(11) DEFAULT NULL',
+                'updated_at' => 'datetime DEFAULT NULL',
+                'updated_by' => 'int(11) DEFAULT NULL',
+                    ), '');
 
 
-        $this->createTable('dropbox_file', array(
-            'id' => 'pk',
-            'link' => 'varchar(512) NOT NULL',
-            'name' => 'varchar(128) NOT NULL',
-            'thumbnail_link' => 'varchar(512) NOT NULL',
-            'object_model' => 'varchar(128) DEFAULT ""',
-            'object_id' => 'varchar(128) DEFAULT ""',
-            'created_at' => 'datetime DEFAULT NULL',
-            'created_by' => 'int(11) DEFAULT NULL',
-            'updated_at' => 'datetime DEFAULT NULL',
-            'updated_by' => 'int(11) DEFAULT NULL',
-                ), '');
+            $this->createTable('dropbox_file', array(
+                'id' => 'pk',
+                'link' => 'varchar(512) NOT NULL',
+                'name' => 'varchar(128) NOT NULL',
+                'thumbnail_link' => 'varchar(512) NOT NULL',
+                'object_model' => 'varchar(128) DEFAULT ""',
+                'object_id' => 'varchar(128) DEFAULT ""',
+                'created_at' => 'datetime DEFAULT NULL',
+                'created_by' => 'int(11) DEFAULT NULL',
+                'updated_at' => 'datetime DEFAULT NULL',
+                'updated_by' => 'int(11) DEFAULT NULL',
+                    ), '');
 
 
-        $this->insert('setting', array('name' => 'warningOnPosting', 'value' => '1', 'module_id' => 'dropbox', 'created_at' => new yii\db\Expression('NOW()'), 'updated_at' => new yii\db\Expression('NOW()')));
+            $this->insert('setting', array('name' => 'warningOnPosting', 'value' => '1', 'module_id' => 'dropbox', 'created_at' => new yii\db\Expression('NOW()'), 'updated_at' => new yii\db\Expression('NOW()')));
+        } catch (Exception $ex) {
+            
+        }
     }
 
     public function down()
